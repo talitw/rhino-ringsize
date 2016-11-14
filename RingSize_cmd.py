482d1c6dde4a93d6376972cbf2e0d506106f6ac5
Tali Weissberger <tali@drawingpost.com>
2016-11-13 20:13:14 -0800

    Change to rhinoscript api

--- RingSize_cmd.py
+++ RingSize_cmd.py
@@ -1,21 +1,11 @@
-from Rhino.DocObjects import *
-from Rhino.Input import *
-from Rhino.Commands import *
-from Rhino.Geometry import *
+import rhinoscriptsyntax as rs
 
-import scriptcontext
-import System.Guid
-#this line with the name of the command:
 __commandname__ = "RingSize"
 def RunCommand( is_interactive ):
-  rc, ringSize = RhinoGet.GetNumber("Ring size (US)?", False, 9)
-  if rc <> Result.Success:
-    return rc
+  ringSize = rs.GetReal("Ring size", 8.5, 0, 16)
+  center = rs.GetPoint("Location")
 
-  center = Point3d(0, 0, 0)
+  plane = rs.MovePlane(rs.ViewCPlane(), center)
   radius = (11.63 + 0.8128 * ringSize) * 0.5
-  c = Circle(center, radius)
-  if scriptcontext.doc.Objects.AddCircle(c)!=System.Guid.Empty:
-    scriptcontext.doc.Views.Redraw()
-    return Result.Success
-  return Result.Failure
+  objId = rs.AddCircle(plane, radius)
+  rs.SelectObject(objId)
